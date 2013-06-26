dbe41b90f76e6a400f0039ab22d4a47eeb90f670
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-06-26 01:13:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MOCK::Application.config.secret_token = '1fab70f1441caf9836604f4786d9116bc7b8ddcceeb6b89778698dc12ca73ab4a44db37acc810bce030df607760050825c2546715d12e4b9ee5e008cec26baa2'

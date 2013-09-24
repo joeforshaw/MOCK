86051b5e8aadc4e464049776c82d29dc4f908952
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-09-24 13:31:08 +0100

    using rails

---
+++ app/assets/javascripts/dendrogram.js
@@ -0,0 +1,69 @@
+var r = 960 / 2;
+
+var cluster = d3.layout.cluster()
+    .size([1000, 1])
+    .sort(null)
+    .value(function(d) { return d.length; })
+    .children(function(d) { return d.branchset; })
+    .separation(function(a, b) { return 1; });
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.x, d.y]; });
+
+function elbow(d, i) {
+  return "M" + d.source.x + "," + d.source.y
+      + "H" + d.target.x + "V" + d.target.y ;
+}
+
+var wrap = d3.select("#dendrogram").append("svg")
+    .attr("width", r * 3)
+    .attr("height", r * 2)
+    .style("-webkit-backface-visibility", "hidden");
+
+var vis = wrap.append("g");
+
+// Calculates child link distance
+function phylo(n, offset) {
+  var multiplier = 270;
+  if (n.length != null) offset += n.length * multiplier;
+  n.y = offset;
+  if (n.children) {
+    n.children.forEach(function(n) {
+      phylo(n, offset);
+    });
+  } else {
+    n.y = 2.6 * multiplier;
+  }
+}
+
+d3.text("life.txt", function(text) {
+  var x = newick.parse(text);
+  var nodes = cluster.nodes(x);
+  phylo(nodes[0], 0);
+
+  // Create links between nodes
+  var link = vis.selectAll("path.link")
+      .data(cluster.links(nodes))
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", elbow);
+
+  var node = vis.selectAll("g.node")
+      .data(nodes.filter(function(n) { return n.x !== undefined; }))
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+
+  node.append("circle")
+      .attr("r", 1);
+
+  // var label = vis.selectAll("text")
+  //     .data(nodes.filter(function(d) { return d.x !== undefined && !d.children; }))
+  //   .enter().append("text")
+  //     .attr("dy", ".31em")
+  //     .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+  //     .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+  //     // .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (r - 170 + 8) + ")rotate(" + (d.x < 180 ? 0 : 180) + ")"; })
+  //     .text(function(d) { return d.name.replace(/_/g, ' '); });
+
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MOCK::Application.config.secret_key_base = 'b7a4653d1d6cb1d3c20cb65b86c960ad88b4da8691b739eacc51e5d695e1a09b3b563181b369671a2975a02f8dec4074390498f0abd0c0888a9bb897436cbcbb'

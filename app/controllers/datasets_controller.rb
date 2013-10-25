20f917e524bad24425fc600c15182741cb784a8c
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-10-25 12:12:34 +0100

    Closes #23, have added delimiter sniffer
    not processing xml or json due to unknown structure that they could be
    uploaded in

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,60 +1,36 @@
 class DatasetsController < ApplicationController
 
+
   before_filter :authenticate_user!
+
 
   def new
     @dataset = Dataset.new
   end
 
-  def create
-    # Create Dataset
-    dataset = Dataset.new(
-      :user_id => current_user.id,
-      :name => params[:dataset][:name],
-      :rows => params[:dataset][:file].tempfile.readlines.size
-    )
-    dataset_columns = -1;
-    datavalues = []
-    if dataset.save
-      sequence_id = 1;
-      CSV.foreach(params[:dataset][:file].tempfile) do |line|
-        # Calculate number of columns
-        if dataset_columns < 0
-          dataset_columns = line.first.split(' ').size
-        end
-        # Create Datapoint
-        datapoint = Datapoint.new(
-          :dataset_id  => dataset.id,
-          :sequence_id => sequence_id
-        )
-        sequence_id += 1
-        
-        if datapoint.save
-          line.first.split(' ').each do |datavalue_string|
-            # Create Datavalue
-            datavalues << Datavalue.new(
-              :value => datavalue_string.to_f,
-              :datapoint_id => datapoint.id
-            )
-          end
-        end
 
-      end
-    end
-    Datavalue.import datavalues
-    dataset.update_attributes(:columns => dataset_columns)
-    redirect_to dataset
+  def create
+    file_extension = File.extname(params[:dataset][:file].original_filename)
+    puts "Extension: #{file_extension}"
+    if file_extension == ".csv"
+      parse_csv(params[:dataset][:file].tempfile)
+    else
+      throw Exception
+    end    
   end
+
 
   def destroy
     Dataset.destroy(params[:id])
     redirect_to :datasets
   end
 
+
   def index
     @body_classes << "datasets-body"
     @datasets = current_user.datasets.order("created_at DESC")
   end
+
 
   def show
     respond_to do |format|
@@ -66,5 +42,81 @@ class DatasetsController < ApplicationController
       end
     end
   end
+
+
+  def parse_csv(csv_file)
+    # Create Dataset
+    dataset = Dataset.new(
+      :user_id => current_user.id,
+      :name => params[:dataset][:name],
+      :rows => params[:dataset][:file].tempfile.readlines.size
+    )
+    dataset_columns = -1;
+    datavalues = []
+    if dataset.save
+      sequence_id = 1;
+      delimiter = delimiter_sniffer(csv_file)
+      File.open(csv_file, "r+") do |file|
+        file.each_line do |line|
+
+          # Calculate number of columns
+          if dataset_columns < 0
+            dataset_columns = line.split(delimiter).size
+          end
+          # Create Datapoint
+          datapoint = Datapoint.new(
+            :dataset_id  => dataset.id,
+            :sequence_id => sequence_id
+          )
+          sequence_id += 1
+          
+          if datapoint.save
+            line.split(delimiter).each do |datavalue_string|
+              datavalues << Datavalue.new(
+                :value => datavalue_string.to_f,
+                :datapoint_id => datapoint.id
+              )
+            end
+          end
+        end
+      end
+    end
+    Datavalue.import datavalues
+    dataset.update_attributes(:columns => dataset_columns)
+    redirect_to dataset
+  end
+
+
+  def delimiter_sniffer(filename)
+    file = File.open(filename)
+    firstline = file.readline
+
+    chosenDelimiter = nil
+    bestDelimiterSize = 0;
+
+    puts "firstline: #{firstline}"
+    [',','\t',' '].each do |delimiter|
+      delimiterSize = firstline.split(delimiter).size
+      puts "'#{delimiter}' has size of #{delimiterSize}"
+      if delimiterSize > bestDelimiterSize
+        chosenDelimiter = delimiter
+        bestDelimiterSize = delimiterSize
+      end
+    end
+
+    if chosenDelimiter.nil?
+      throw Exception
+    end
+    puts "Delimiter: '#{chosenDelimiter}'"
+    
+    return chosenDelimiter
+
+  end
+
+
+  def parse_xml
+
+  end
+
 
 end

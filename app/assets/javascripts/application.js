81f667dc395d79f13467a99d5b6fbeff04efba9a
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-11-04 16:25:48 +0000

    styled some forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,13 @@
 //= require d3/d3.solution
 //= require d3/d3.solutionfront
 
+$('.file-field-input').change(function(e) {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('.file-field-text').text(filename);
+});
+
+$('confirmation-required').on('click', function() {
+  return confirm('Are you sure you want to delete #{dataset.name}?');
+});
--- app/assets/javascripts/datasets.js
+++
@@ -1,6 +0,0 @@
-$('#dataset-upload').change(function(e) {
-    var filepath = this.value;
-    var m = filepath.match(/([^\/\\]+)$/);
-    var filename = m[1];
-    $('.dataset-filename').text(filename);
-});

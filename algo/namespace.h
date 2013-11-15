c28368099eefd89ec91c760b1a9ee0c24333f508
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-11-15 17:11:58 +0000

    initial commit

---
+++ algo/namespace.h
@@ -0,0 +1,17 @@
+// For use with rb++
+namespace to_wrap {
+  #include "clalg.h"
+  #include "clustering.h"
+  #include "conf.h"
+  #include "databin.h"
+  #include "epsilon.h"
+  #include "evaluation.h"
+  #include "gaclust.h"
+  #include "gasdev.h"
+  #include "kmeans.h"
+  #include "mockvis.h"
+  #include "pca.h"
+  #include "pesa2.h"
+  #include "random.h"
+  #include "tmatrix.h"
+}

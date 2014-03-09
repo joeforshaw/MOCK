79e1c3c5a195a8e48bea28faacb6b0528065c3f2
Joe Forshaw <joe.d.forshaw@gmail.com>
2014-03-09 13:40:27 +0000

    updated test code

--- app/controllers/clusters_controller.rb
+++
@@ -1,9 +0,0 @@
-class ClustersController < ApplicationController
-
-  before_filter :authenticate_user!
-
-  def show
-    @cluster = Cluster.find(params[:id])
-  end
-
-end
--- app/controllers/control_solutions_controller.rb
+++ app/controllers/control_solutions_controller.rb
@@ -3,13 +3,10 @@ class ControlSolutionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    run = Run.find(params[:id])
+    @run = Run.find(params[:id])
     respond_to do |format|
-      format.html do
-
-      end
       format.csv do
-        render text: run.control_solution_csv
+        render text: @run.control_solution_csv
       end
     end
   end
